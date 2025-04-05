ca86d6cefde275f1fbf53a6623d1fdaa80856e80
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2025-04-04 21:38:15 -0700

    Update Wk07_Factorial.java
    fixed, gives desired output, passes https://codecheck.io/files/230316210692t39etbx6i6xxdisb0x0vktl 's check

--- GuessGame/src/Wk07_Factorial.java
+++ GuessGame/src/Wk07_Factorial.java
@@ -2,15 +2,44 @@ import java.util.Scanner;
 
 public class Wk07_Factorial {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        int muilt = 0;
+        Scanner s = new Scanner (System.in);
 
+        int start, stop, count;
 
-        System.out.print("Please enter a number: ");
-        int input = scanner.nextInt();
+        System.out.print("Enter a starting number (greater than 0): ");
+        start = s.nextInt();
+        while(start <= 0){
+            System.out.print("Enter a starting number (greater than 0): ");
+            start = s.nextInt();
+        } //this 'while loop' is the gate for acceptable answers
 
-        while(input == 0){
-            System.out.println(input * input--);
+        count = start;
+        
+        System.out.print("Enter a stopping number (greater than " + start + "): ");
+        stop = s.nextInt();
+        while(stop <= start){
+            System.out.print("Enter a stopping number (greater than " + start + "): ");
+            stop = s.nextInt();
+        } //this 'while loop' is the gate for acceptable answers
+
+        int factors = 0; //to announce how many factors a num has
+
+        while(count != (stop + 1)){
+            System.out.print("Factors for " + count + ":");
+            for(int i = 1; i <= count; i++){
+                if(count % i == 0){
+                    System.out.print(" " + i);
+                    factors++;
+                }
+            }
+            System.out.println(); // for new line
+            System.out.println(count + " has " + factors + " factors");
+            
+            System.out.println(); // for new line
+
+            count++;
+            factors = 0;
         }
+        
     }
 }
